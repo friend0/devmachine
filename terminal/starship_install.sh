1c4e7840863b7626943f81a0cd597e4f7d77071e
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2024-06-22 14:29:39 -0700

    add shell checking for starship install

--- terminal/starship_install.sh
+++ terminal/starship_install.sh
@@ -1,7 +1,13 @@
+#!/usr/bin/env zsh
+
 curl -sS https://starship.rs/install.sh | sh
 
-# Add this to .zshrc
-eval "$(starship init zsh)"
 
-# Add this to .bashrc
-eval "$(starship init bash)"
+if [ "$SHELL" = "/bin/zsh" ] || [ "$SHELL" = "/usr/bin/zsh" ]; then
+    # Add this to .zshrc
+    eval "$(starship init zsh)"
+else
+    # Add this to .bashrc
+    eval "$(starship init bash)"
+fi
+
